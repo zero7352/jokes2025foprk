7f1f6ffaab77b5778c05ef60134342539a00e372
zero7352 <jsierra17@cps.edu>
2026-02-05 18:25:18 +0000

    jokes2025foprk

--- main.py
+++ main.py
@@ -4,34 +4,40 @@
 # make this performance task ready for submission
 # To give the user a fun experience hearing knock knock jokes
 
-joke = input("Do you want to hear a joke? ")
-if joke == "no":
-    print("Okay suit yourself!")
-while joke == "yes":
-    print("Great, Let's Play")
-    question = input("Do you want to hear a joke about robbers, tanks, or pencils? ")
-    if question == "robbers":
-        input("Knock Knock ")
-        input("Calder")
-        print("Calder police - I've been robbed!")
-        joke = input("Do you want to hear another joke or are you finished? ")
-    elif question == "tanks":
-        input("Knock Knock ")
-        input("Tank ")
-        input("You are welcome! ")
-        joke = input("Do you want to hear another joke or are you finished? ")
-    elif question == "pencils":
-        input("Knock Knock ")
-        input("Broken pencil ")
-        input("Nevermind, it's pointless! ")
-        joke = input("Do you want to hear another joke or are you finished? ")
-if joke == "finished":
-    rate = int(input("Please rate our game 1-10! "))
-    final_score = int(rate * 10)
-    print(str(final_score) + " percent satisfaction rate")
-    friend = input("Would you recommend this game to a friend? ")
+# List of joke categories (Requirement: List)
+categories = ["robbers", "tanks", "pencils"]
 
-    if friend == "yes" or friend == "maybe":
-        print("Thanks, we appreciate it. ")
+# Procedure with a parameter (Requirement: Procedure/Function)
+def tell_joke(choice):
+    # Selection (if/else) inside the procedure
+    if choice == categories[0]:
+        input("Knock Knock... ")
+        input("Calder... ")
+        print("Calder police - I've been robbed!")
+    elif choice == categories[1]:
+        input("Knock Knock... ")
+        input("Tank... ")
+        print("You are welcome!")
+    elif choice == categories[2]:
+        input("Knock Knock... ")
+        input("Broken pencil... ")
+        print("Nevermind, it's pointless!")
+
+# Main Program
+start = input("Do you want to hear a joke? (yes/no): ").lower()
+
+# Iteration (Requirement: Loop)
+while start == "yes":
+    print(f"Options: {categories}")
+    question = input("Pick a category: ").lower()
+    
+    if question in categories:
+        tell_joke(question) # Calling the procedure
     else:
-        print("Sorry you did not enjoy it. ")
+        print("That's not an option!")
+    
+    start = input("Hear another? (yes/no): ").lower()
+
+# Final Scoring
+rate = int(input("Rate this 1-10: "))
+print(f"{rate * 10}% satisfaction rate!")
